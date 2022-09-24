4a2f2c66f3751ad996e8ffc2f5730232664fa857
notomo <notomo.motono@gmail.com>
2022-09-24 02:44:58 +0000

    Small refactor test

--- lua/tracebundler/test/helper.lua
+++ lua/tracebundler/test/helper.lua
@@ -2,11 +2,8 @@ local plugin_name = vim.split((...):gsub("%.", "/"), "/", true)[1]
 local helper = require("vusted.helper")
 
 helper.root = helper.find_plugin_root(plugin_name)
-local runtimepath = vim.o.runtimepath
 
-function helper.before_each()
-  vim.o.runtimepath = runtimepath
-end
+function helper.before_each() end
 
 function helper.after_each()
   helper.cleanup()
@@ -18,45 +15,5 @@ function helper.path_filter(path)
   local matched = path:match("/busted/")
   return not matched
 end
-
-function helper.set_lines(str)
-  vim.api.nvim_buf_set_lines(0, 0, -1, false, vim.split(str, "\n"))
-end
-
-function helper.get_row(pattern)
-  local saved = vim.fn.winsaveview()
-  local row = vim.fn.search(pattern)
-  if row == 0 then
-    error(("not found pattern: `%s`"):format(pattern))
-  end
-  vim.fn.winrestview(saved)
-  return row
-end
-
-function helper.use_parsers()
-  vim.o.runtimepath = helper.root .. "/spec/lua/nvim-treesitter," .. vim.o.runtimepath
-  vim.cmd.runtime([[plugin/nvim-treesitter.*]])
-end
-
-function helper.install_parser(language)
-  helper.use_parsers()
-  if not vim.treesitter.language.require_language(language, nil, true) then
-    vim.cmd.TSInstallSync(language)
-  end
-end
-
-local asserts = require("vusted.assert").asserts
-
-asserts.create("test_values"):register_same(function(tests)
-  return vim.tbl_map(function(test)
-    local row = test.scope_node:start()
-    return { name = test.name, row = row + 1 }
-  end, tests)
-end)
-
-asserts.create("test_value"):register_same(function(test)
-  local row = test.scope_node:start()
-  return { name = test.name, row = row + 1 }
-end)
 
 return helper
